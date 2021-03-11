c6c0e73e2acd23c5e5e0eea084d88d042da27f86
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-11 23:41:56 +0000

    updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,50 @@
 const request = require('request');
 // const fs = require ('fs');
-const breed = process.argv[2];
-
-const breedFetcher = function(breed) {
-  const sliceBreed = breed.slice(0, 4);
-  const link = `https://api.thecatapi.com/v1/images/search?breed_id=${sliceBreed}`;
-  request(link, (error, response, body) =>{
+// const breed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  // const sliceBreed = breedName.slice(0, 4);
+  const link = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(link, (error, response, body) => {
     if (error) {
-      console.log('Fail to fetch', error);
-      return;
-    }
-    // fs.readFile (breed, body, (error) => {
-    let data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log(`Breed: ${breed} not found`);
+      callback(error, null);
     } else {
-      console.log(data[0]);
+      const data = JSON.parse(body);
+      const desc = data[0];
+      // const desc = breed.description;
+      // console.log(breed)
+      if (desc) {
+        callback(null, desc.description);
+      // console.log(desc)
+      } else {
+        callback("Breed doesn't exist", null);
+      }
     }
   });
 };
 
-breedFetcher(breed);
+
+
+
+
+
+
+// const breedFetcher = function(breed) {
+//   const sliceBreed = breed.slice(0, 4);
+//   const link = `https://api.thecatapi.com/v1/images/search?breed_id=${sliceBreed}`;
+//   request(link, (error, response, body) =>{
+//     if (error) {
+//       console.log('Fail to fetch', error);
+//       return;
+//     }
+//     // fs.readFile (breed, body, (error) => {
+//     let data = JSON.parse(body);
+//     if (data[0] === undefined) {
+//       console.log(`Breed: ${breed} not found`);
+//     } else {
+//       console.log(data[0]);
+//     }
+//   });
+// };
+
+
+module.exports = { fetchBreedDescription };
